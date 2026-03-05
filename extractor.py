79967791cca5d787d495598f5badd43ad9e7caaf
Cool_Show <22648256+Cool-ShowTTV@users.noreply.github.com>
2026-03-05 04:23:48 -0500

    Create docs for the fucntions
    Also add how to read the releases file

--- extractor.py
+++ extractor.py
@@ -6,9 +6,29 @@ import zipfile, shutil, os
 OUTPUT_DIR = "output"
 
 # region Functions
-def get_versions():
+def get_versions()->list:
+    """Returns a list of version file names
+
+    Returns:
+        list: A list of version file names
+    """
+    
+    ### Tidal API Info
+    #   API return information is in the format of:
+    #   <hash> <version_file_name> <file_size>
+    #   
+    #   The "RELEASES" file also starts with a "U+FEFF" which is a Zero Width unicode,
+    #   So if you are parsing this file for the hash to verify make sure to only get A-Z and 0-9 characters
+    #   
+    #   There are two types of versions,
+    #   - full.nupkg: The full package, which contains the full .asar file we want
+    #   - delta.nupkg: A smaller update package, which only contains a .diff/.bsdiff file, which isn't helpful for us
+    ###
     response = get("https://download.tidal.com/desktop/windows/RELEASES")
-    if response.status_code != 200: exit(1)
+    if response.status_code != 200:
+        print("Failed to fetch versions, status code:", response.status_code)
+        print("Feel free to try again later if it's down.")
+        exit(1)
 
     versions = []
     for line in response.text.splitlines():
@@ -18,7 +38,16 @@ def get_versions():
 
     return versions
 
-def download_file(url, dest):
+def download_file(url:str, dest:str):
+    """Downloads a file from the URL to dest location
+
+    Args:
+        url (str): URL to the asset
+        dest (str): File path to the output
+
+    Raises:
+        ValueError: Raised if either are set as None
+    """
     if url is None or dest is None: raise ValueError("URL and destination must not be None")
     response = get(url)
     if response.status_code == 200:
@@ -28,10 +57,21 @@ def download_file(url, dest):
         print(f"Failed to download {url}")
 
 def clean_temp():
+    """Removes and recreates the temp folder
+    """
     shutil.rmtree("temp")
     os.makedirs("temp")
 
-def extract_asar(nupkg_file, out_name):
+def extract_asar(nupkg_file:str, out_name:str):
+    """Searches for and extracts the ASAR file from the nupkg
+
+    Args:
+        nupkg_file (str): The update nupkg containing an ASAR file
+        out_name (str): The file to output the ASAR to
+
+    Raises:
+        FileNotFoundError: If the ASAR file isn't found
+    """
     with zipfile.ZipFile(nupkg_file, 'r') as zip_ref:
         asar_files = [f for f in zip_ref.namelist() if f.endswith('.asar')]
         if not asar_files:
